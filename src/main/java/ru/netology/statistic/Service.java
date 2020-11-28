9186d4b415ba36c9674ec3d42689290e28b55676
sleypneer <sleypneer@mail.ru>
2020-11-28 16:48:27 +0300

    mod Service

--- src/main/java/ru/netology/statistic/Service.java
+++ src/main/java/ru/netology/statistic/Service.java
@@ -5,7 +5,7 @@ public class Service {
     /**
      * Calculate index of max income    *    * @param incomes — array of incomes    * @return — index of first max value
      */
-    publiclong findMax(long[] incomes) {
+    public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
