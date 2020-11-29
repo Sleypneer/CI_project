994c050f39c21e473c825812396b4ee660fa7e03
sleypneer <sleypneer@mail.ru>
2020-11-29 16:02:12 +0300

    added test

---
+++ src/test/java/ru/netology/statistic/ServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+    @Test
+    void findMax() {
+        Service service = new Service();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+}
